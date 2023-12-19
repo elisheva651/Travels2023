7534db05f7891fb793518103be16b1f72a848a1d
elisheva <eli65193@gmail.com>
2023-12-19 09:58:00 +0200

    19.12.23

--- server/script.js
+++ server/script.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 const cors = require('cors')
 const Trip=require("./TripSchema")
 const bodyParser = require('body-parser');
-
+const axios = require('axios');
 
 const express = require('express')
 
@@ -12,12 +12,24 @@ app.use(bodyParser.json())
 const port = 5000
 const multer = require('multer');
 const TripSchema = require('./TripSchema');
+const { Axios } = require('axios');
 const upload =multer({});
 
 const uri = 'mongodb+srv://trips_db:tripsdb@travels.4gvojse.mongodb.net/?retryWrites=true&w=majority&tls=true';
 mongoose.connect(uri).then((x)=>{console.log("connected")}).catch(error=>{console.log("catch", error)})
 
+// Access-Control-Allow-Origin: https://en.wikipedia.org/w
 
+
+
+// region consts
+const wik1 = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=" 
+const wik2 = "&format=json"
+const wiki1 = "https://en.wikipedia.org/w/api.php?origin=*&action=parse&prop=text&page="
+const wiki2 ="&section=4&format=json&formatversion=2&callback=?"
+const weather_api_key = "04f7642090575e9b9e62e2f1252f28aa"
+const weather_url_1 = "https://api.openweathermap.org/data/2.5/weather?q="
+const weather_url_2 = `&units=Metric&appid=${weather_api_key}`
 
 
 
@@ -114,7 +126,6 @@ app.post('/api/editPost', async (req, res) => {
  app.post('/api/addTrip', async (req, res) => {
   try{ 
     
-    console.log("server side, dataTrip [imgs].length that came in req.body", req.body["imgs"].length)
     const newTripDoc = new Trip(req.body)
     const trip1 = await newTripDoc.save();
     if(trip1 !== newTripDoc)
@@ -130,6 +141,44 @@ app.post('/api/editPost', async (req, res) => {
   }
   
 });
+
+
+app.get('/weather',async (req, res) => {
+  try{
+    const region = JSON.parse(req.query["region"])
+    const url = weather_url_1 + region + weather_url_2
+    // console.log(url)
+    const _result = await axios.get(url)
+    const result = {
+      temp:Math.round(_result.data.main.temp), 
+      wind:_result.data.wind.speed,
+      humidity:_result.data.main.humidity, 
+      location:_result.data.name,
+      imgSrc:`http://openweathermap.org/img/w/${_result.data.weather[0].icon}.png`, 
+      descriptionImg:_result.data.weather[0].description
+    }
+
+    res.status(200).send(JSON.stringify(result))
+    return 
+  }catch(error){
+    console.log(error, " error in weather fetch")
+
+  }
+})
+
+app.post('/wiki',async (req, res) => {
+  try{
+    const region = JSON.parse(req.query["region"])
+    console.log("region" , region)
+    const url = wik1 + region + wik2
+    console.log(url)
+    const _result = await axios.get(url)
+    res.status(200).send()
+    return 
+  }catch(error){
+    console.log(error, " error in wiki fetch")
+  }
+})
 
 // app.newTrip('/country/:countryId',(req, res) => {
 
